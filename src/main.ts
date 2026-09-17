7d9ffd8bc2b94bc03acef8cb8de3af443541c999
toshi1209 <th236toshikazuh@gmail.com>
2026-09-17 01:10:47 -0700

    walking cat silhouette marks

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
 import './style.css'
+
+void fetch('/neko.b64')
+  .then((res) => {
+    if (!res.ok) throw new Error('neko')
+    return res.text()
+  })
+  .then((b64) => {
+    document.documentElement.style.setProperty(
+      '--neko',
+      `url("data:image/png;base64,${b64.trim()}")`,
+    )
+  })
+  .catch(() => {})
