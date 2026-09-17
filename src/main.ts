56e41b50d4c450c1a271e96303010740af49e459
toshi1209 <th236toshikazuh@gmail.com>
2026-09-16 20:29:38 -0700

    Load generated Sakura cat onto the white portfolio

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
 import './style.css'
+
+const img = document.querySelector('.sakura img')
+
+if (img instanceof HTMLImageElement) {
+  void fetch('/sakura.b64')
+    .then((res) => {
+      if (!res.ok) throw new Error('sakura image missing')
+      return res.text()
+    })
+    .then((b64) => {
+      img.src = 'data:image/webp;base64,' + b64.trim()
+    })
+    .catch(() => {
+      img.alt = '案内役のねこ、さくら'
+    })
+}
