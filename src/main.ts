483daee5f871e1132a19b8df05ddfbb34b1506fa
toshi1209 <th236toshikazuh@gmail.com>
2026-09-17 01:13:21 -0700

    embed walking cat silhouette

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,1 @@
 import './style.css'
-
-void Promise.all(
-  ['/n1.b64', '/n2.b64'].map((path) =>
-    fetch(path).then((res) => {
-      if (!res.ok) throw new Error('neko')
-      return res.text()
-    }),
-  ),
-)
-  .then((parts) => {
-    document.documentElement.style.setProperty(
-      '--neko',
-      `url("data:image/png;base64,${parts.join('').replace(/\s+/g, '')}")`,
-    )
-  })
-  .catch(() => {})
